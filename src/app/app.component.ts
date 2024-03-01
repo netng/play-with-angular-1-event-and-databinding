48b63d867f111eef15272acc6189c63bbb9be000
nandang <net.nandang@gmail.com>
2024-03-01 10:24:11 +0700

    angular 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angularProject1';
+
+  name = '';
+  email = '';
+  phone = '';
+  address = '';
+  qualifications = [{school: '', degree: '', year: ''}];
+  isSubmitted = false;
+
+  addQualification(): void {
+    this.qualifications.push({school: '', degree: '', year: ''});
+  }
+
+  removeQualification(i: number): void {
+    this.qualifications.splice(i, 1);
+  }
+
+  onSubmit() {
+    this.isSubmitted = true;
+  }
+
+  onEdit() {
+    this.isSubmitted = false;
+  }
 }
